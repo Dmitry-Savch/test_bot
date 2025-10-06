eb32747a51c670cfd1b75f68acfd9a6146b96a13
Dmitry-Savch <savchdimon@gmail.com>
2025-10-06 20:20:36 +0300

    updated utils

--- modifiers/sat_form_modifier.py
+++ modifiers/sat_form_modifier.py
@@ -10,16 +10,6 @@ def render_sat_form(
     template_path: str = "templates/sat_form.png",
     output_path: str = "output/result.png"
 ) -> str:
-    """
-    INFO: Основна функція модифікатора.
-    Накладає 4 текстових параметри на шаблон SAT форми.
-
-    Returns: шлях до збереженого результату
-    """
-    # TODO: визначити точні координати з OCR або вручну
-    # TODO: підібрати шрифти (можна використати дефолтні спочатку)
-    # TODO: підібрати розміри шрифтів
-
     base_img = Image.open(template_path).convert("RGBA")
 
     font_family = "fonts/OpenSans-Semibold.ttf"
@@ -27,7 +17,6 @@ def render_sat_form(
     font_size = 50
     kr_default = 1.0
 
-    # NOMBRE DEL BENEFICIARIO - поле dropdown, ліворуч
     base_img = text_draw_execute(
         base_image=base_img,
         font_sz=font_size,
@@ -39,7 +28,6 @@ def render_sat_form(
         kerning=kr_default
     )
 
-    # NUMERO DE CUENTA - поле dropdown, ліворуч
     base_img = text_draw_execute(
         base_image=base_img,
         font_sz=font_size,
@@ -51,7 +39,6 @@ def render_sat_form(
         kerning=kr_default
     )
 
-    # IMPORTE DEL IMPUESTO - поле dropdown, ліворуч
     base_img = text_draw_execute(
         base_image=base_img,
         font_sz=font_size,
@@ -63,7 +50,6 @@ def render_sat_form(
         kerning=kr_default
     )
 
-    # VALOR ENVIADO - текстове поле, ліворуч
     base_img = text_draw_execute(
         base_image=base_img,
         font_sz=font_size,
@@ -75,12 +61,9 @@ def render_sat_form(
         kerning=kr_default
     )
 
-    # Оптимізуємо розмір для швидшої відправки
     width, height = base_img.size
-    # Зменшуємо до 70% від оригіналу (баланс якість/розмір)
     new_size = (int(width * 0.7), int(height * 0.7))
     base_img_resized = base_img.resize(new_size, Image.LANCZOS)
-    base_img_resized.convert("RGB").save(
-        output_path, quality=85, optimize=True)
+    base_img_resized.convert("RGB").save(output_path, quality=85, optimize=True)
 
     return output_path
